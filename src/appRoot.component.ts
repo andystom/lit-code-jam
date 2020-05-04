9371067103eda8e82a35a6c3e90c94a388ae3dfb
Ondrej Tomek <ONTO@local.tmvse.com>
2020-05-04 15:01:29 +0100

    Chnages color when position of mouse moves over button.
    Co-authored-by: DOST@tmvse.com <DOST@tmvse.com>
    Co-authored-by: Kym Eden <kyed@tmvse.com>
    Co-authored-by: frgo@tmvse.com <frgo@tmvse.com>

--- src/appRoot.component.ts
+++ src/appRoot.component.ts
@@ -30,6 +30,4 @@ export class AppRoot extends LitElement {
         `;
     }
 
-    // ------------------------------
-
 }
--- src/components/shyButton.component.ts
+++ src/components/shyButton.component.ts
@@ -7,8 +7,21 @@ import styles from './shyButton.component.scss';
 @customElement('shy-button-component')
 export class ShyButtonComponent extends LitElement {
 
+    @property()
+    private distance : number = 0;
+    private oldX : number = 0;
+    private oldY : number = 0;
+
     constructor() {
         super();
+        
+        // this.position = this.getBoundingClientRect();
+
+        window.addEventListener('mousemove', e => {
+            this.distance = e.offsetX - this.oldX;
+            this.oldX = e.offsetX;
+            this.oldY = e.offsetY;
+        });
     }
 
     // ----------------------------------------------------------
@@ -18,14 +31,26 @@ export class ShyButtonComponent extends LitElement {
     static get styles() { return styles as CSSResult; }
 
     public render() {
+        var isMovingLeft = this.distance > 0;
+        if (isMovingLeft){
         return html`
             <button 
                 onmouseover="this.style.backgroundColor = '#FFB6C1'"
                 onmouseleave="this.style.backgroundColor = null" 
                 type="button"> 
-                Something 
+                "I'm shy"
             </button>
         `;
+        }
+
+        return html`
+        <button 
+            onmouseover="this.style.backgroundColor = '#bb1830'"
+            onmouseleave="this.style.backgroundColor = null" 
+            type="button"> 
+            "I'm shy"
+        </button>
+    `;
     }
 }
 
